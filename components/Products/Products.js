bae4f28e5f2010ce6967676f6e5b48cf8517ab55
boryantheone <boryantheone228@yandex.ru>
2022-04-02 12:03:42 +0300

    button click

---
+++ components/Products/Products.js
@@ -0,0 +1,59 @@
+class Products {
+	constructor() {
+		this.classNameActive = 'products-elements__btn_active';
+		this.labelAdd = 'Добавить в корзину';
+		this.labelRemove = 'Удалить из корзины';
+	}
+
+	handleSetLocationStorage(element, id) {
+		const {pushProduct, products} = localStorageUtil.putProducts(id);
+		
+		if (pushProduct) {
+			element.classList.add(this.classNameActive);
+			element.innerHTML = this.labelRemove;
+		} else {
+			element.classList.remove(this.classNameActive);
+			element.innerHTML = this.labelAdd;
+		}
+
+	}
+
+	render() {
+		const productsStorage = localStorageUtil.getProducts();
+		let htmlCatalog = '';
+
+		CATALOG.forEach(({id , name, author, price, img}) => {
+			let activeClass = '';
+			let activeText = '';
+
+			if (productsStorage.indexOf(id) === -1) {
+				activeText = this.labelAdd;
+			} else {
+				activeClass = ' ' + this.classNameActive;
+				activeText = this.labelRemove;
+			}
+
+			htmlCatalog += `
+				<li class="products-elements">
+					<span class="products-elements__name">${name}</span>
+					<span class="products-elements__author">${author}</span>
+					<img class="products-elements__img" src='${img}' />
+					<span class="products-elements__price">
+						💌${price.toLocaleString()} RUB
+					</span>
+					<button class="products-elements__btn${activeClass}" onclick="productsPage.handleSetLocationStorage(this, '${id}');">${activeText}</button>
+				</li>
+			`;
+		});
+		const html = `
+			<ul class="products-container">
+				${htmlCatalog}
+			</ul>
+		`;
+
+		ROOT_PRODUCTS.innerHTML = html;
+	}
+}
+
+const productsPage = new Products();
+productsPage.render();
---
+++ constants/catalog.js
@@ -0,0 +1,30 @@
+const CATALOG = [
+	{
+		id: 'el1',
+		name: 'О дивный новый мир',
+		author: 'Хаксли Олдос',
+		img: 'https://img4.labirint.ru/rc/8871e516da7183b8504c2c82a37d084a/220x340/books43/420220/cover.png?1612967102',
+		price: 424,
+	},
+	{
+		id: 'el2',
+		name: 'Мы',
+		author: 'Замятин Евгений Иванович',
+		img: 'https://img3.labirint.ru/rc/e6b24c9c31b70c0ab70e03a99ee09736/220x340/books79/786431/cover.jpg?1610774755',
+		price: 343,
+	},
+	{
+		id: 'el3',
+		name: 'Десять негритят',
+		author: 'Кристи Агата',
+		img: 'https://img3.labirint.ru/rc/80d5e064d8501736fac85ca4b41180b1/220x340/books71/707251/cover.jpg?1564211913',
+		price: 329,
+	},
+	{
+		id: 'el4',
+		name: 'Бабий Яр',
+		author: 'Кузнецов Анатолий Владимирович',
+		img: 'https://img3.labirint.ru/rc/fe4bdb17f7cbaec6780930bc5915a789/220x340/books70/698553/cover.png?1575304810',
+		price: 1175,
+	},
+]
